ce4d495eb1c1f54ff8618f1ac13edd166a8c86f5
Samarth Agarwal <samarthagarwal@live.com>
2019-05-20 19:07:12 +0530

    Change PhonePe identifier

--- lib/flutter_upi.dart
+++ lib/flutter_upi.dart
@@ -7,11 +7,11 @@ class FlutterUpiApps {
   static const String PayTM = "net.one97.paytm";
   static const String GooglePay = "com.google.android.apps.nbu.paisa.user";
   static const String BHIMUPI = "in.org.npci.upiapp";
-  static const String PHONEPE = "com.phonepe.app";
+  static const String PhonePe = "com.phonepe.app";
   static const String MiPay = "com.mipay.wallet.in";
   static const String AmazonPay = "in.amazon.mShop.android.shopping";
   static const String TrueCallerUPI = "com.truecaller";
-  static const String MyAirtelUPI = "com.myairtelapp";  
+  static const String MyAirtelUPI = "com.myairtelapp";
 }
 
 class FlutterUpiResponse {
